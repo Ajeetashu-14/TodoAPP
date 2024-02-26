e2e2ed70d9f0a52d03a7586c322e08dab6ac3c53
Ajeetashu-14 <rohilla.ajeet12@gmail.com>
2024-02-26 12:09:57 +0530

    Frontend base_code

---
+++ frontend/src/components/Todo.jsx
@@ -0,0 +1,8 @@
+
+export function Todo(){
+    return <div>
+        <h2>Learn something new</h2>
+        <h3>Learn react from 5 to 7 pm</h3>
+        <button>Mark as complete</button>
+    </div>
+}
